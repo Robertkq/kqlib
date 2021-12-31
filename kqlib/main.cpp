6021b0cffbca37e2d8e9bc6adfc17a03f0a702df
Robert Vuia <robertidfk69@gmail.com>
2021-12-31 19:26:24 +0200

    Update

--- kqlib/main.cpp
+++ kqlib/main.cpp
@@ -1,28 +1,14 @@
+﻿#include <iostream>
 #include "kqlib.h"
-#include <iostream>
 
 
 int main()
 {
-    kq::vector<int> A({1,2,3,4,5,6});
-    kq::vector<int> a(A.begin() + 1, A.end() - 1 );
-    for (auto& e : a)
+    kq::vector<int> v({ 1,2,3,4,5, });
+    kq::vector<int> v2(v.begin() + 1, v.end() - 1);
+    for (auto& e : v2)
     {
         std::cout << e << " ";
     }
-    std::cout << "\n";
-    kq::list<int> B({ 1,2,3,4,5,6 });
-    kq::list<int> b(++B.begin(), --B.end());
-    for (auto& e : b)
-    {
-        std::cout << e << " ";
-    }
-    std::cout << "\n";
-    kq::deque<int> C({ 1,2,3,4,5,6 });
-    kq::deque<int> c(C.begin() + 1, C.end() - 1);
-    for (auto& e : c)
-    {
-        std::cout << e << " ";
-    }
-    std::cout << "\n";
+    return 0;
 }
