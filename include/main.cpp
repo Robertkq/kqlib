2345d36d1a8b32484d1a5ff1bf15c2142b1dbc6f
Robert Vuia <robertidfk69@gmail.com>
2022-01-02 02:30:32 +0200

    Added include\main.cpp for testing

---
+++ include/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> v({ 1,2,3 });
+    for (auto& e : v)
+    {
+        std::cout << e << " ";
+    }
+    v.at(4);
+    std::cout << "test";
+    return 0;
+}
