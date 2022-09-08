d80b5f8ef66e3251739a8670499da0ac05ef781e
Robert Vuia <robertidfk69@gmail.com>
2022-09-08 04:52:13 +0300

    added less_equal & greater_equal

--- include/kqlib/compare.h
+++ include/kqlib/compare.h
@@ -6,7 +6,7 @@ namespace kq
 
 	namespace details
 	{
-		//Tag dispatch for equal_compare_impl functions to return TRUE if lhs == rhs
+		//Tag dispatch for equal_compare_impl functions for returning lhs == rhs using best available operators
 		struct eql_no_operator {};
 		struct eql_not_equal : eql_no_operator {};
 		struct eql_equal : eql_not_equal {};
@@ -29,30 +29,7 @@ namespace kq
 		auto equal_compare_impl(const T& lhs, const T& rhs, eql_no_operator)
 			-> bool = delete;
 
-		struct sort_partition_eql_no_operator {};
-		struct sort_partition_eql_not_equal : sort_partition_eql_no_operator {};
-		struct sort_partition_eql_equal : sort_partition_eql_not_equal {};
-		struct sort_partition_eql_operator_tag : sort_partition_eql_equal {};
-
-
-		template<typename T>
-		auto sort_partition_equal_compare_impl(const T& lhs, const T& rhs, sort_partition_eql_equal)
-			-> decltype(lhs == rhs)
-		{
-			return lhs == rhs;
-		}
-		template<typename T>
-		auto sort_partition_equal_compare_impl(const T& lhs, const T& rhs, sort_partition_eql_not_equal)
-			-> decltype(lhs == rhs)
-		{
-			return !(lhs != rhs);
-		}
-		template<typename T>
-		auto sort_partition_equal_compare_impl(const T& lhs, const T& rhs, sort_partition_eql_no_operator)
-		{
-			return false; // so that we will not swap in the if statement of sort_partition
-		}
-
+		//Tag dispatch for less_compare_impl functions for returning lhs != rhs using best available operators
 		struct not_eql_no_operator {};
 		struct not_eql_equal : not_eql_no_operator {};
 		struct not_eql_not_equal : not_eql_equal {};
@@ -76,31 +53,31 @@ namespace kq
 		auto not_equal_compare_impl(const T& lhs, const T& rhs, not_eql_no_operator)
 			-> bool = delete;
 
-		//Tag dispatch for less_compare_impl fucntions to return TRUE if lhs < rhs or in bad cases lhs <= rhs
+		//Tag dispatch for less_compare_impl functions to return for returning lhs < rhs using best available operators
 		struct less_no_operator {};
-		struct less_has_greater_or_equal : less_no_operator {};
-		struct less_has_smaller_or_equal : less_has_greater_or_equal {};
-		struct less_has_greater : less_has_smaller_or_equal {};
-		struct less_has_greater_or_equal_NE : less_has_greater {};
-		struct less_has_greater_or_equal_E : less_has_greater_or_equal_NE {};
-		struct less_has_smaller_or_equal_NE : less_has_greater_or_equal_NE {};
-		struct less_has_smaller_or_equal_E : less_has_smaller_or_equal_NE {};
-		struct less_has_greater_NE : less_has_smaller_or_equal_E {};
-		struct less_has_greater_E : less_has_greater_NE {};
-		struct less_has_smaller : less_has_greater_E {};
-		struct less_operator_tag : less_has_smaller {};
+		struct less_greater_or_equal : less_no_operator {};
+		struct less_smaller_or_equal : less_greater_or_equal {};
+		struct less_greater : less_smaller_or_equal {};
+		struct less_greater_or_equal_NE : less_greater {};
+		struct less_greater_or_equal_E : less_greater_or_equal_NE {};
+		struct less_smaller_or_equal_NE : less_greater_or_equal_NE {};
+		struct less_smaller_or_equal_E : less_smaller_or_equal_NE {};
+		struct less_greater_NE : less_smaller_or_equal_E {};
+		struct less_greater_E : less_greater_NE {};
+		struct less_smaller : less_greater_E {};
+		struct less_operator_tag : less_smaller {};
 
 
 		template<typename T>
-		auto less_compare_impl(const T& lhs, const T& rhs, less_has_smaller)
+		auto less_compare_impl(const T& lhs, const T& rhs, less_smaller)
 			-> decltype(lhs < rhs)
 		{
 			return (lhs < rhs);
 		}
 
 		template<typename T>
-		auto less_compare_impl(const T& lhs, const T& rhs, less_has_greater_E)
-			-> decltype(lhs > rhs&& lhs == rhs)
+		auto less_compare_impl(const T& lhs, const T& rhs, less_greater_E)
+			-> decltype(lhs > rhs && lhs == rhs)
 		{
 			if (lhs == rhs)
 			{
@@ -109,7 +86,7 @@ namespace kq
 			return !(lhs > rhs);
 		}
 		template<typename T>
-		auto less_compare_impl(const T& lhs, const T& rhs, less_has_greater_NE)
+		auto less_compare_impl(const T& lhs, const T& rhs, less_greater_NE)
 			-> decltype(lhs > rhs&& lhs != rhs)
 		{
 			if (lhs != rhs)
@@ -119,7 +96,7 @@ namespace kq
 			return false;
 		}
 		template<typename T>
-		auto less_compare_impl(const T& lhs, const T& rhs, less_has_smaller_or_equal_E)
+		auto less_compare_impl(const T& lhs, const T& rhs, less_smaller_or_equal_E)
 			->decltype(lhs <= rhs && lhs == rhs)
 		{
 			if (lhs == rhs)
@@ -129,7 +106,7 @@ namespace kq
 			return lhs <= rhs;
 		}
 		template<typename T>
-		auto less_compare_impl(const T& lhs, const T& rhs, less_has_smaller_or_equal_NE)
+		auto less_compare_impl(const T& lhs, const T& rhs, less_smaller_or_equal_NE)
 			->decltype(lhs <= rhs && lhs != rhs)
 		{
 			if (lhs != rhs)
@@ -139,7 +116,7 @@ namespace kq
 			return false;
 		}
 		template<typename T>
-		auto less_compare_impl(const T& lhs, const T& rhs, less_has_greater_or_equal_E)
+		auto less_compare_impl(const T& lhs, const T& rhs, less_greater_or_equal_E)
 			->decltype(lhs >= rhs && lhs == rhs)
 		{
 			if (lhs == rhs)
@@ -149,7 +126,7 @@ namespace kq
 			return !(lhs >= rhs);
 		}
 		template<typename T>
-		auto less_compare_impl(const T& lhs, const T& rhs, less_has_greater_or_equal_NE)
+		auto less_compare_impl(const T& lhs, const T& rhs, less_greater_or_equal_NE)
 			->decltype(lhs >= rhs && lhs != rhs)
 		{
 			if (lhs != rhs)
@@ -159,19 +136,19 @@ namespace kq
 			return false;
 		}
 		template<typename T>
-		auto less_compare_impl(const T& lhs, const T& rhs, less_has_greater)
+		auto less_compare_impl(const T& lhs, const T& rhs, less_greater)
 			-> decltype(lhs > rhs)
 		{
 			return !(lhs > rhs);
 		}
 		template<typename T>
-		auto less_compare_impl(const T& lhs, const T& rhs, less_has_smaller_or_equal)
+		auto less_compare_impl(const T& lhs, const T& rhs, less_smaller_or_equal)
 			-> decltype(lhs <= rhs)
 		{
 			return lhs <= rhs;
 		}
 		template<typename T>
-		auto less_compare_impl(const T& lhs, const T& rhs, less_has_greater_or_equal)
+		auto less_compare_impl(const T& lhs, const T& rhs, less_greater_or_equal)
 			-> decltype(lhs >= rhs)
 		{
 			return !(lhs >= rhs);
@@ -181,31 +158,31 @@ namespace kq
 			-> bool = delete;
 
 
-		//Tag dispatch for grt_compare_impl fucntions to return TRUE if lhs > rhs or in bad cases lhs >= rhs
+		//Tag dispatch for grt_compare_impl functions for returning lhs > rhs using best available operators
 		struct grt_no_operator {};
-		struct grt_has_smaller_or_equal : grt_no_operator {};
-		struct grt_has_greater_or_equal : grt_has_smaller_or_equal {};
-		struct grt_has_smaller : grt_has_greater_or_equal {};
-		struct grt_has_smaller_or_equal_NE : grt_has_greater_or_equal {};
-		struct grt_has_smaller_or_equal_E : grt_has_smaller_or_equal_NE {};
-		struct grt_has_greater_or_equal_NE : grt_has_smaller_or_equal_E {};
-		struct grt_has_greater_or_equal_E : grt_has_greater_or_equal_NE {};
-		struct grt_has_smaller_NE : grt_has_greater_or_equal_E {};
-		struct grt_has_smaller_E : grt_has_smaller_NE {};
-		struct grt_has_greater : grt_has_smaller_E {};
-		struct grt_operator_tag : grt_has_greater {};
+		struct grt_smaller_or_equal : grt_no_operator {};
+		struct grt_greater_or_equal : grt_smaller_or_equal {};
+		struct grt_smaller : grt_greater_or_equal {};
+		struct grt_smaller_or_equal_NE : grt_greater_or_equal {};
+		struct grt_smaller_or_equal_E : grt_smaller_or_equal_NE {};
+		struct grt_greater_or_equal_NE : grt_smaller_or_equal_E {};
+		struct grt_greater_or_equal_E : grt_greater_or_equal_NE {};
+		struct grt_smaller_NE : grt_greater_or_equal_E {};
+		struct grt_smaller_E : grt_smaller_NE {};
+		struct grt_greater : grt_smaller_E {};
+		struct grt_operator_tag : grt_greater {};
 
 
 		template<typename T>
-		auto grt_compare_impl(const T& lhs, const T& rhs, grt_has_greater)
+		auto grt_compare_impl(const T& lhs, const T& rhs, grt_greater)
 			-> decltype(lhs > rhs)
 		{
 			//std::cout << "min_element() lhs > rhs\n";
 			return (lhs > rhs);
 		}
 		template<typename T>
-		auto grt_compare_impl(const T& lhs, const T& rhs, grt_has_smaller_E)
-			-> decltype(lhs < rhs&& lhs == rhs)
+		auto grt_compare_impl(const T& lhs, const T& rhs, grt_smaller_E)
+			-> decltype(lhs < rhs && lhs == rhs)
 		{
 			//std::cout << "min_element() lhs < rhs && lhs == rhs\n";
 			if (lhs == rhs)
@@ -215,8 +192,8 @@ namespace kq
 			return !(lhs < rhs);
 		}
 		template<typename T>
-		auto grt_compare_impl(const T& lhs, const T& rhs, grt_has_smaller_NE)
-			-> decltype(lhs < rhs&& lhs != rhs)
+		auto grt_compare_impl(const T& lhs, const T& rhs, grt_smaller_NE)
+			-> decltype(lhs < rhs && lhs != rhs)
 		{
 			//std::cout << "min_element() lhs < rhs && lhs != rhs\n";
 			if (lhs != rhs)
@@ -226,7 +203,7 @@ namespace kq
 			return false;
 		}
 		template<typename T>
-		auto grt_compare_impl(const T& lhs, const T& rhs, grt_has_greater_or_equal_E)
+		auto grt_compare_impl(const T& lhs, const T& rhs, grt_greater_or_equal_E)
 			->decltype(lhs >= rhs && lhs == rhs)
 		{
 			//std::cout << "min_element() lhs >= rhs && lhs == rhs\n";
@@ -237,7 +214,7 @@ namespace kq
 			return lhs >= rhs;
 		}
 		template<typename T>
-		auto grt_compare_impl(const T& lhs, const T& rhs, grt_has_greater_or_equal_NE)
+		auto grt_compare_impl(const T& lhs, const T& rhs, grt_greater_or_equal_NE)
 			->decltype(lhs >= rhs && lhs != rhs)
 		{
 			//std::cout << "min_element() lhs >= rhs && lhs != rhs\n";
@@ -248,7 +225,7 @@ namespace kq
 			return false;
 		}
 		template<typename T>
-		auto grt_compare_impl(const T& lhs, const T& rhs, grt_has_smaller_or_equal_E)
+		auto grt_compare_impl(const T& lhs, const T& rhs, grt_smaller_or_equal_E)
 			->decltype(lhs <= rhs && lhs == rhs)
 		{
 			//std::cout << "min_element() lhs <= rhs && lhs == rhs\n";
@@ -259,7 +236,7 @@ namespace kq
 			return !(lhs <= rhs);
 		}
 		template<typename T>
-		auto grt_compare_impl(const T& lhs, const T& rhs, grt_has_smaller_or_equal_NE)
+		auto grt_compare_impl(const T& lhs, const T& rhs, grt_smaller_or_equal_NE)
 			->decltype(lhs <= rhs && lhs != rhs)
 		{
 			//std::cout << "min_element() lhs <= rhs && lhs != rhs\n";
@@ -270,21 +247,21 @@ namespace kq
 			return false;
 		}
 		template<typename T>
-		auto grt_compare_impl(const T& lhs, const T& rhs, grt_has_smaller)
+		auto grt_compare_impl(const T& lhs, const T& rhs, grt_smaller)
 			-> decltype(lhs < rhs)
 		{
 			//std::cout << "min_element() lhs < rhs\n";
 			return !(lhs < rhs);
 		}
 		template<typename T>
-		auto grt_compare_impl(const T& lhs, const T& rhs, grt_has_greater_or_equal)
+		auto grt_compare_impl(const T& lhs, const T& rhs, grt_greater_or_equal)
 			-> decltype(lhs >= rhs)
 		{
 			//std::cout << "min_element() lhs >= rhs\n";
 			return lhs >= rhs;
 		}
 		template<typename T>
-		auto grt_compare_impl(const T& lhs, const T& rhs, grt_has_smaller_or_equal)
+		auto grt_compare_impl(const T& lhs, const T& rhs, grt_smaller_or_equal)
 			-> decltype(lhs <= rhs)
 		{
 			//std::cout << "min_element() lhs <= rhs\n";
@@ -293,7 +270,152 @@ namespace kq
 		template<typename T>
 		auto grt_compare_impl(const T& lhs, const T& rhs, grt_no_operator)
 			-> bool = delete;
-	}
+
+		//Tag dispatch for less_equal_compare_impl functions returning lhs <= rhs using best available operators
+		struct less_equal_no_operator {};
+		struct less_equal_greater_NE : less_equal_no_operator {};
+		struct less_equal_greater_E : less_equal_greater_NE {};
+		struct less_equal_less_NE : less_equal_greater_E {};
+		struct less_equal_less_E : less_equal_less_NE {};
+		struct less_equal_greater_or_equal : less_equal_less_E {};
+		struct less_equal_less_or_equal : less_equal_greater_or_equal {};
+		struct less_equal_operator_tag : less_equal_less_or_equal {};
+
+		template<typename T>
+		auto less_equal_compare_impl(const T& lhs, const T& rhs, less_equal_less_or_equal)
+			-> decltype(lhs <= rhs)
+		{
+			return lhs <= rhs;
+		}
+
+		template<typename T>
+		auto less_equal_compare_impl(const T& lhs, const T& rhs, less_equal_greater_or_equal)
+			-> decltype(lhs >= rhs)
+		{
+			return !(lhs >= rhs);
+		}
+
+		template<typename T>
+		auto less_equal_compare_impl(const T& lhs, const T& rhs, less_equal_less_E)
+			-> decltype(lhs < rhs && lhs == rhs)
+		{
+			if (lhs == rhs)
+			{
+				return true;
+			}
+			return lhs < rhs;
+		}
+
+		template<typename T>
+		auto less_equal_compare_impl(const T& lhs, const T& rhs, less_equal_less_NE)
+			-> decltype(lhs < rhs && lhs != rhs)
+		{
+			if (lhs != rhs)
+			{
+				return  lhs < rhs;
+			}
+			return true;
+		}
+
+		template<typename T>
+		auto less_equal_compare_impl(const T& lhs, const T& rhs, less_equal_greater_E)
+			-> decltype(lhs > rhs && lhs == rhs)
+		{
+			if (lhs == rhs)
+			{
+				return true;
+			}
+			return !(lhs > rhs);
+		}
+
+		template<typename T>
+		auto less_equal_compare_impl(const T& lhs, const T& rhs, less_equal_greater_NE)
+			-> decltype(lhs > rhs && lhs != rhs)
+		{
+			if (lhs != rhs)
+			{
+				return !(lhs > rhs);
+			}
+			return true;
+		}
+
+		template<typename T>
+		auto less_equal_compare_impl(const T& lhs, const T& rhs, less_equal_no_operator)
+			-> bool = delete;
+
+		//Tag dispatch for less_equal_compare_impl functions returning lhs >= rhs using best available operators
+		struct greater_equal_no_operator {};
+		struct greater_equal_less_NE : greater_equal_no_operator {};
+		struct greater_equal_less_E : greater_equal_less_NE {};
+		struct greater_equal_greater_NE : greater_equal_less_E {};
+		struct greater_equal_greater_E : greater_equal_greater_NE {};
+		struct greater_equal_less_or_equal : greater_equal_greater_E {};
+		struct greater_equal_greater_or_equal : greater_equal_less_or_equal {};
+		struct greater_equal_operator_tag : greater_equal_greater_or_equal {};
+
+		template<typename T>
+		auto greater_equal_compare_impl(const T& lhs, const T& rhs, greater_equal_greater_or_equal)
+			-> decltype(lhs >= rhs)
+		{
+			return lhs >= rhs;
+		}
+
+		template<typename T>
+		auto greater_equal_compare_impl(const T& lhs, const T& rhs, greater_equal_less_or_equal)
+			-> decltype(lhs <= rhs)
+		{
+			return !(lhs <= rhs);
+		}
+
+		template<typename T>
+		auto greater_equal_compare_impl(const T& lhs, const T& rhs, greater_equal_greater_E)
+			-> decltype (lhs > rhs && lhs == rhs)
+		{
+			if (lhs == rhs)
+			{
+				return true;
+			}
+			return lhs > rhs;
+		}
+
+		template<typename T>
+		auto greater_equal_compare_impl(const T& lhs, const T& rhs, greater_equal_greater_NE)
+			-> decltype (lhs > rhs && lhs != rhs)
+		{
+			if (lhs != rhs)
+			{
+				return lhs > rhs;
+			}
+			return true;
+		}
+
+		template<typename T>
+		auto greater_equal_compare_impl(const T& lhs, const T& rhs, greater_equal_less_E)
+			-> decltype (lhs < rhs && lhs == rhs)
+		{
+			if (lhs == rhs)
+			{
+				return true;
+			}
+			return !(lhs < rhs);
+		}
+
+		template<typename T>
+		auto greater_equal_compare_impl(const T& lhs, const T& rhs, greater_equal_less_NE)
+			-> decltype (lhs < rhs && lhs != rhs)
+		{
+			if (lhs != rhs)
+			{
+				return !(lhs < rhs);
+			}
+			return true;
+		}
+
+		template<typename T>
+		auto greater_equal_compare_impl(const T& lhs, const T& rhs, greater_equal_no_operator)
+			-> bool = delete;
+
+	} // namespace details
 
 	template<typename T>
 	struct equal_to
@@ -301,15 +423,6 @@ namespace kq
 		bool operator()(const T& lhs, const T& rhs) const
 		{
 			return details::equal_compare_impl(lhs, rhs, details::eql_operator_tag{});
-		}
-	};
-
-	template<typename T>
-	struct sort_partition_equal_to
-	{
-		bool operator()(const T& lhs, const T& rhs) const
-		{
-			return details::sort_partition_equal_compare_impl(lhs, rhs, details::sort_partition_eql_operator_tag{});
 		}
 	};
 
@@ -337,6 +450,24 @@ namespace kq
 		bool operator()(const T& lhs, const T& rhs) const
 		{
 			return details::grt_compare_impl(lhs, rhs, details::grt_operator_tag{});
+		}
+	};
+
+	template<typename T>
+	struct less_equal
+	{
+		bool operator()(const T& lhs, const T& rhs) const
+		{
+			return details::less_equal_compare_impl(lhs, rhs, details::less_equal_operator_tag);
+		}
+	};
+
+	template<typename T>
+	struct greater_equal
+	{
+		bool operator()(const T& lhs, const T& rhs) const
+		{
+			return details::greater_equal_compare_impl(lhs, rhs, details::greater_equal_operator_tag);
 		}
 	};
 
